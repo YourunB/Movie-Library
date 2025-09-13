1f98136b42a83fe70f3c7b8ed27cf5c0d9a71010
Yury Butskevich <124175026+YourunB@users.noreply.github.com>
2025-09-13 13:02:26 +0300

    feat: create header (#9)
    * feat: create header
    
    * feat: add logic for header
    
    * feat: show header from app
    
    * refactor: update header style
    
    * refactor: update header and style
    
    * feat: create menu by stadalone
    
    * refactor: change folder for layouts
    
    * feat: create UI actions for menu
    
    * feat: creaete reducer and selectors
    
    * refactor: change menu by standalone
    
    * feat: add menu to header
    
    * feat: create def routes and home page for work menu
    
    * refactor: change menu style
    
    * feat: add btn close and update menu style
    
    * refactor: update menu style

---
+++ src/app/layouts/main-layout/header/menu/menu.ts
@@ -0,0 +1,20 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { closeMenu } from '../../../../../store/ui.actions';
+import { Store } from '@ngrx/store';
+
+@Component({
+  selector: 'app-menu',
+  imports: [CommonModule, RouterModule],
+  standalone: true,
+  templateUrl: './menu.html',
+  styleUrl: './menu.scss'
+})
+export class Menu {
+  private store = inject(Store);
+
+  close() {
+    this.store.dispatch(closeMenu());
+  }
+}
