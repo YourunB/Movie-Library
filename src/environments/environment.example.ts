3bd54961d1a96841b3e2abb6d398e375369fafdd
RamanMilashevich <roma.milashevich@gmail.com>
2025-09-13 11:23:24 +0200

    init: configure environments

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  tmdb: {
+    apiBaseUrl: 'https://api.themoviedb.org/3',
+    imageBaseUrl: 'https://image.tmdb.org/t/p/',
+    apiKey: '<<YOUR_TMDB_API_KEY>>',
+    language: 'en-US',
+    region: 'PL',
+    includeImageLanguage: 'pl,en,null'
+  },
+};
