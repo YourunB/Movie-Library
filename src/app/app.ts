1fe34ce62d6c0690336c06dc3361b814f4a40a48
yourunb <bxyura@gmail.com>
2025-09-14 20:19:02 +0300

    feat: create box with developers links

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { Header } from './layouts/main-layout/header/header';
+import { Footer } from './layouts/main-layout/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Header],
+  imports: [Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {}
-
