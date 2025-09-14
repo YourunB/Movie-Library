9c028f19d219034e08ff7826e99582b92acd0aaf
Anastasiya Andronova <anastasiya5.88@mail.ru>
2025-09-15 01:43:58 +0300

    Squashed commit of the following:
    commit fad91d1835b1ede1be657a60f74475f1334128fb
    Author: Yury Butskevich <124175026+YourunB@users.noreply.github.com>
    Date:   Sun Sep 14 22:00:04 2025 +0300
    
        feat: add footer (#15)
    
        * feat: create footer and move header for header folder
    
        * refactor: move components position from folders
    
        * feat: create box with developers links
    
        * feat: add box with rs school info and images for box
    
        * feat: add year and project info to footer
    
        * refactor: change padding from footer
    
    commit 1f98136b42a83fe70f3c7b8ed27cf5c0d9a71010
    Author: Yury Butskevich <124175026+YourunB@users.noreply.github.com>
    Date:   Sat Sep 13 13:02:26 2025 +0300
    
        feat: create header (#9)
    
        * feat: create header
    
        * feat: add logic for header
    
        * feat: show header from app
    
        * refactor: update header style
    
        * refactor: update header and style
    
        * feat: create menu by stadalone
    
        * refactor: change folder for layouts
    
        * feat: create UI actions for menu
    
        * feat: creaete reducer and selectors
    
        * refactor: change menu by standalone
    
        * feat: add menu to header
    
        * feat: create def routes and home page for work menu
    
        * refactor: change menu style
    
        * feat: add btn close and update menu style
    
        * refactor: update menu style

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { Header } from './layouts/main-layout/header/header';
+import { Footer } from './layouts/main-layout/footer/footer';
 import { RouterOutlet } from '@angular/router';
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Header, RouterOutlet],
+  imports: [Header, Footer, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {}
-
