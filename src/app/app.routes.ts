d6adfcca33db40590c2a4383ebf55dbd2cf10a82
yourunb <bxyura@gmail.com>
2025-09-18 21:07:26 +0300

    feat: create movie page and add route for page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,13 @@ import { SignupPage } from './pages/signup/signup.page';
 import { SigninPage } from './pages/signin/signin.page';
 import { authGuard } from './shared/guards/auth.guard';
 import { NotFoundPage } from './pages/not-found.page/not-found.page';
+import { MoviePage } from './pages/movie/movie';
 
 export const routes: Routes = [
   { path: '', component: HomePage, canActivate: [authGuard] },
   { path: 'signup', component: SignupPage },
   { path: 'signin', component: SigninPage },
+  { path: 'movie/:id', component: MoviePage },
   { path: '**', component: NotFoundPage },
 ];
+
