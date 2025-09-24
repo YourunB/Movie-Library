755617cdc129841d4881e0f1807a4c5c19aea582
Anastasiya Andronova <anastasiya5.88@mail.ru>
2025-09-25 00:30:24 +0300

    feat: add tranlation of menu

--- src/app/layouts/main-layout/header/menu/menu.ts
+++ src/app/layouts/main-layout/header/menu/menu.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { closeMenu } from '../../../../../store/ui/ui.actions';
 import { Store } from '@ngrx/store';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-menu',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, TranslatePipe],
   standalone: true,
   templateUrl: './menu.html',
   styleUrl: './menu.scss',
