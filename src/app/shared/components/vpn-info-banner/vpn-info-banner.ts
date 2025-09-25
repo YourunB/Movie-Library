158a8facf366bb6ce15180bd24c0c6745cb11386
Anastasiya Andronova <anastasiya5.88@mail.ru>
2025-09-25 23:31:31 +0300

    feat: add translation of components

--- src/app/shared/components/vpn-info-banner/vpn-info-banner.ts
+++ src/app/shared/components/vpn-info-banner/vpn-info-banner.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { RegionInfoService } from '../../services/region-info.service';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-vpn-info-banner',
   standalone: true,
-  imports: [CommonModule, MatIconModule, MatButtonModule],
+  imports: [CommonModule, MatIconModule, MatButtonModule, TranslatePipe],
   templateUrl: './vpn-info-banner.html',
   styleUrl: './vpn-info-banner.scss',
 })
