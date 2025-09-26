e01ae4fb1ef65373993760fdea340d4597a00052
RamanMilashevich <roma.milashevich@gmail.com>
2025-09-26 12:07:51 +0200

    feat: refactor watchlist page to use watchlist signals and improve movie card styles

--- src/app/pages/watchlist/watchlist.page.ts
+++ src/app/pages/watchlist/watchlist.page.ts
@@ -1,9 +1,10 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIcon } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 import { WatchlistService } from '../../shared/services/watchlist.service';
-import { Observable, take } from 'rxjs';
+import { Observable } from 'rxjs';
+import { WatchlistSignalsStore } from '../../shared/services/watchlist-signals.store';
 import { TmdbMovie } from '../../../models/dashboard';
 import { MatCardModule } from '@angular/material/card';
 import { AuthService } from '../../shared/services/auth.service';
@@ -30,37 +31,24 @@ import { TranslatePipe } from '@ngx-translate/core';
   templateUrl: './watchlist.page.html',
   styleUrl: './watchlist.page.scss',
 })
-export class WatchlistPage implements OnInit {
+export class WatchlistPage {
   public tmdb = inject(TmdbService);
   authService = inject(AuthService);
   watchListService = inject(WatchlistService);
+  watchlistSignals = inject(WatchlistSignalsStore);
 
-  movies$!: Observable<TmdbMovie[]>;
   isAuth$: Observable<boolean>;
 
   constructor() {
     this.isAuth$ = this.authService.authenticatedSubject;
   }
 
-  ngOnInit(): void {
-    this.movies$ = this.watchListService.watchlist$;
-  }
-
   toggleFavorite(movie: MovieCardModel): void {
-    this.watchListService
-      .isMovieInWatchlist(movie.id)
-      .pipe(take(1))
-      .subscribe((isInWatchlist) => {
-        if (isInWatchlist) {
-          this.watchListService.removeMovie(movie.id);
-        } else {
-          const normalized: TmdbMovie = {
-            ...(movie as unknown as TmdbMovie),
-            poster_path: movie.poster_path ?? null,
-            release_date: movie.release_date ?? null,
-          };
-          this.watchListService.addMovie(normalized);
-        }
-      });
+    const normalized: TmdbMovie = {
+      ...(movie as unknown as TmdbMovie),
+      poster_path: movie.poster_path ?? null,
+      release_date: movie.release_date ?? null,
+    };
+    this.watchlistSignals.toggle(normalized);
   }
 }
