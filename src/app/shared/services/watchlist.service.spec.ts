62f83461c651abce6d023f4de6e1358fd68b3fdc
yourunb <bxyura@gmail.com>
2025-09-27 17:38:35 +0300

    feat: add tests for not found page and watchlist service

--- src/app/shared/services/watchlist.service.spec.ts
+++ src/app/shared/services/watchlist.service.spec.ts
@@ -1,12 +1,22 @@
 import { TestBed } from '@angular/core/testing';
-
 import { WatchlistService } from './watchlist.service';
+import { Store } from '@ngrx/store';
 
 describe('WatchlistService', () => {
   let service: WatchlistService;
 
+  const storeMock = {
+    dispatch: jasmine.createSpy('dispatch'),
+    select: jasmine.createSpy('select'),
+  };
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: Store, useValue: storeMock },
+      ],
+    });
+
     service = TestBed.inject(WatchlistService);
   });
 
